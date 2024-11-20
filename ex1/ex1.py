9887e3f28173d6cfde451bc6f0235c5a514c0dc9
lennehberg <lenny.agizim@gmail.com>
2024-11-20 16:16:05 +0200

    final solution for exercise 1

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -4,10 +4,27 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-def convert_vid_arr_to_grayscale(vid_arr):
+
+def __power_transform(frame, maxinpval):
+    """
+    Power transform on a single frame, gamma = 7 for very bright frames,
+    assuming 255 intensity levels
+    """
+    # power transform algorithm: r = c * (s ** gamma)
+    c = 255 / np.log(1 + maxinpval) if 1 + maxinpval != 0 else 1
+    gamma = 7
+    frame = frame / 255
+    frame = (frame ** gamma) * c
+    frame = 255 * (frame / np.max(frame))
+    return frame
+
+
+def convert_vid_arr_to_grayscale(vid_arr, video_type):
     """
     Converts a video array (numpy array) to a grayscale video array (numpy array)
+    if video is of type 2, run a quick power transform on the frame to equalize the intensities
     :param vid_arr: a numpy array representing the video
+    :param video_type: type of video to convert
     :return: a grayscale video numpy array
     """
     # initiate a frames array for grayscale
@@ -16,119 +33,117 @@ def convert_vid_arr_to_grayscale(vid_arr):
     # iterate over video frame (index 0), convert to grayscale image using PIL
     for frame in vid_arr:
         grayscale_frame = Image.fromarray(frame).convert("L")
-        grayscale_frame_arr = np.array(grayscale_frame)
+        grayscale_frame_arr = np.array(grayscale_frame).astype(np.uint16)
+        if video_type == '2':
+            grayscale_frame_arr = __power_transform(grayscale_frame_arr, np.max(grayscale_frame_arr))
         grayscale_frames.append(grayscale_frame_arr)
 
     # return frames list
     return grayscale_frames
 
-def generate_cum_hists(grayscale_vid_arr):
+
+def __apply_lut(hist, lut):
+    """ Applies lookup table to grayscale frame to compute new equalized frame"""
+    eq_hist = np.zeros(hist.shape)
+    # eq_frame[i , j] = lut[grayscale_frame[i, j]]
+    for i, val in enumerate(lut):
+        eq_hist[int(val)] += hist[i]
+    return eq_hist
+
+
+def __calculate_hist(frame, bins_num=256):
+    """calculates the histogram of the frame"""
+    hist, _ = np.histogram(frame, bins=bins_num, range=(0, bins_num - 1))
+    return hist
+
+
+def __normalize_cum_hist(cum_hist, pixel_count, level=256):
+    return (level - 1) * cum_hist / pixel_count
+
+
+def __linear_stretch(np_arr, new_min, new_max):
+    """Linearly stretch the array to new min and new max"""
+    # stretched[k] = (new_max - new_min) * (a[k] - a[m]) / (a[M] - a[m]) + new_min
+    old_min = np.argmax(np_arr != 0)
+    old_max = np.max(np_arr)
+
+    numer = np_arr - old_min
+    denom = old_max - old_min
+
+    # math for uniform frames
+    if denom == 0:
+        return np.full_like(np_arr, new_min)
+
+    stretched = (numer / denom) * (new_max - new_min) + new_min
+    # make sure values under 0 are 0
+    stretched[stretched < 0] = 0
+    return stretched
+
+
+def __histogram_difference(hist1, hist2):
+    """Calculates the vector distance of the histograms difference."""
+    return np.sum(np.abs(hist1 - hist2))
+
+
+def __equalize_frame_histogram(frame, levels=256):
+    """Equalize rhe frame's histogram and return the new frame"""
+    # compute histogram for frame
+    hist = __calculate_hist(frame, levels)
+    # compute cumulative histogram for cdf
+    cum_hist = np.cumsum(hist)
+    # normalize cdf
+    normed_cdf = __normalize_cum_hist(cum_hist, frame.size, levels)
+    # perform linear stretching if min value isn't 0 and max value isn't 255
+    # if np.min(normed_cdf) != 0 or np.max(normed_cdf) != levels - 1:
+    normed_cdf = __linear_stretch(normed_cdf, 0, levels - 1)
+
+    # round values in cdf to get mappings for lut
+    cdf = np.round(normed_cdf)
+    return __apply_lut(hist, cdf)
+
+
+def detect_cuts(grayscale_vid_arr, video_type):
     """
-    generate a Cumulative hists list from a numpy grayscale array
-    :param grayscale_vid_arr: a numpy array representing a grayscale video
-    :return: cumulative histogram list by frames
+    Detect a cut in a video by finding the maximum difference between cumulative histograms
+    :param grayscale_vid_arr: grayscale numpy array of video to find cut
+    :param video_type: type of video to find cut
+    :return: a tuple of the indexes where the cut happened
     """
-    hists_and_bins = []
-    cum_hists = []
-    # iterate over frames and generate a cumulative histogram for each frame
-    for frame in grayscale_vid_arr:
-        temp_hist, bins = np.histogram(frame, bins=256, range=(0, 255))
-        hists_and_bins.append((temp_hist, bins))
-        cum_hist = np.cumsum(temp_hist)
-        # append cum hist to cum hists list
-        cum_hists.append(cum_hist)
-
-    return hists_and_bins, cum_hists
-
-
-def create_lut(cum_hist, N):
-    # create an empty lookup table
-    lut = np.zeros(256)
-    # using T(k) = round(N * ((C(k) - C(m)) / ((C(N) - c(m)))
-    for k in range(256):
-        lut[k] = round(N * ((cum_hist[k] - cum_hist[np.argmax(cum_hist != 0)]) /
-                            (cum_hist[N] - cum_hist[np.argmax(cum_hist != 0)])))
-        # normalize values in case of overflow
-        if lut[k] < 0:
-            lut[k] = 0
-
-    return lut
-
-
-def apply_lut(frame, lut):
-    return np.interp(frame.flatten(), np.arange(256), lut).reshape(frame.shape)
-
-
-def equalize_histogram(gray_frame, cum_hist):
-    """
-    run a simple histogram equalization algorithm
-    :param gray_frame: original grayscale frame to equalize
-    :param cum_hist: cumulative frame histogram
-    :return: equalized frame
-    """
-    # normalize the cum_hists to get a normalized cdf
-    normed_cdf = cum_hist * (255 / cum_hist[-1])
-    # create a look-up table and map values accordingly
-    lut = create_lut(cum_hist, 255)
-    eq_frame = apply_lut(gray_frame, lut)
-    # return frame to uint8 type
-    eq_frame = eq_frame.astype(np.uint8)
-    eq_hist, _ = np.histogram(eq_frame, bins=256, range=(0, 256))
-    return eq_frame, eq_hist
-
-
-def equalize_video_frame(grayscale_vid_arr, cum_hists):
-    """
-    iterate over all frame in grayscale video array and run the equalization algorithm
-    :param grayscale_vid_arr:
-    :param cum_hists:
-    :return: array of equalized frames
-    """
-    # init a frame count to keep track of needed hists
-    frame_count = 0
-    eq_frames = []
-    eq_hists = []
-    for frame in grayscale_vid_arr:
-        # run the equalization algorithm on each frame
-        eq_frame, eq_hist = equalize_histogram(frame, cum_hists[frame_count])
-        # append generated frame to frame list
-        eq_frames.append(eq_frame)
-        eq_hists.append(eq_hist)
-        # increment frame count for next frame
-        frame_count += 1
-
-    return eq_frames, eq_hists
-
-
-def get_hist_diff(l_hist, r_hist):
-    return np.sum(np.abs(l_hist - r_hist))
-
-
-def get_hist_paris_diff(eq_hists):
-    """
-    get the hist differences of consecutive frame
-    :param eq_hists:
-    :return: array[{(prev_frame_num, cur_frame_num), diff}]
-    """
-    pairs_diff = []
-    for k in range(len(eq_hists) - 2):
-        # compute the diff and store in paris_diff
-        pairs_diff.append(((k, k + 1), get_hist_diff(eq_hists[k], eq_hists[k + 1])))
-    return pairs_diff
-
-
-def get_cut_diff_indexes(hists_pairs_diffs):
-    cut_indexes = []
+    first_frame = grayscale_vid_arr[0]
+    frame_shape = grayscale_vid_arr[0].shape
     max_diff = 0
-    max_pair = 0
-    for pair in hists_pairs_diffs:
-        print(f"diff is {pair[1]}")
-        if pair[1] > max_diff:
-            max_pair = pair
-            max_diff = pair[1]
+    max_cut_ind = (0, 0)
+    # get the equalized cumulative histogram of the first frame (needed for category 2)
+    eq_prev_hist = __equalize_frame_histogram(first_frame)
+    prev_cdf = np.cumsum(eq_prev_hist)
 
-    return max_pair
+    # iterate over rest of the frame and get their differences from previous frames
+    for i in range(1, len(grayscale_vid_arr)):
+        # if video type is 1, just check differences between cumulative histograms
+        if video_type == '1':
+            hist_diff = __histogram_difference(__calculate_hist(grayscale_vid_arr[i - 1]),
+                                               __calculate_hist(grayscale_vid_arr[i]))
+        else:  # if video type is 2, compare equalized cumulative histograms
+            # equalize the current frame's histogram, and get it's cumulative histogram
+            eq_cur_hist = __equalize_frame_histogram(grayscale_vid_arr[i])
+            cur_cdf = np.cumsum(eq_cur_hist)
+            # get the difference between cur cumulative histogram and prev cumulative histogram
+            hist_diff = __histogram_difference(prev_cdf, cur_cdf)
+            # set prev cdf to cur cdf and continue to rest of frames
+            prev_cdf = cur_cdf
 
+        # debug print: check histogram differences between frames
+        # print(f"{hist_diff} at {i - 1, i}")
+        # check if cdf distance is bigger than max
+        if hist_diff > max_diff:
+            # set max to current cdf distance
+            max_diff = hist_diff
+            # set the max cut index to cur and prev index of frames
+            max_cut_ind = (i - 1, i)
+
+    # debug print: check where cut was found and at what distance
+    # print(f"cut detected at frames {max_cut_ind} with diff {max_diff}")
+    return max_cut_ind
 
 
 def main(video_path, video_type):
@@ -139,48 +154,22 @@ def main(video_path, video_type):
     :return: a tuple of integers representing the frame number for which the scene cut was detected
     (i.e. the last frame index of the first scene and the first frame index of the second scene)
     """
-    print("starting cut detection...")
-    # open video from video path and make sure it's a valid path
+    # open video from vido path
     video = media.read_video(video_path)
-    if video is None:
-        print(f"{video_path} not found")
-    print("video found!")
-    # convert the video to grayscale for easier histogram manipulation
+    # convert video to grayscale
     vid_arr = np.array(video)
-    grayscale_vid_arr = convert_vid_arr_to_grayscale(vid_arr)
-    # generate a cumulative histogram array from frames
-    hists_and_bins, cum_hists = generate_cum_hists(grayscale_vid_arr)
-    # run a histogram equalization
-    eq_vid, eq_vid_hists = equalize_video_frame(grayscale_vid_arr, cum_hists)
-    # compute histogram differences for pairs in video
-    hists_pairs_diffs = get_hist_paris_diff(eq_vid_hists)
-    # check where the highest changes in histograms are
-    cut_indexes = get_cut_diff_indexes(hists_pairs_diffs)
-    # return (first scene index, last scene index)
-    # print(f"cuts at: {', '.join([str(index) for index in cut_indexes])}")
-    grayscale_vid_arr = np.array(grayscale_vid_arr)
-    print(grayscale_vid_arr.shape)
-    print(cut_indexes)
-    # Create a figure with 1 row and 2 columns
-    plt.figure(figsize=(10, 5))
-
-    # First subplot (first image)
-    plt.subplot(1, 2, 1)
-    plt.imshow(grayscale_vid_arr[cut_indexes[0][0]], cmap='gray')  # Use cmap='gray' for grayscale images
-    plt.title('Image 1')
-    plt.axis('off')  # Turn off axis
-
-    # Second subplot (second image)
-    plt.subplot(1, 2, 2)
-    plt.imshow(grayscale_vid_arr[cut_indexes[0][1]], cmap='gray')  # Use cmap='gray' for grayscale images
-    plt.title('Image 2')
-    plt.axis('off')  # Turn off axis
-
-    # Show the plot
-    plt.tight_layout()
-    plt.show()
+    grayscale_vid_arr = np.array(convert_vid_arr_to_grayscale(vid_arr, video_type))
+    # detect a cut in video by checking for the maximal difference between histograms
+    # of consecutive frames
+    return detect_cuts(grayscale_vid_arr, video_type)
 
 
-if __name__ == "__main__":
-    video_path = sys.argv[1]
-    main(video_path=video_path, video_type=1)
+# if __name__ == "__main__":
+#     video_path = sys.argv[1]
+#     video_type = sys.argv[2]
+#     # video = media.read_video(video_path)
+#     # vid_arr = np.array(video)
+#     # show_video_frames(vid_arr)
+#     # debug to see where frame cut is
+#     print(main(video_path=video_path, video_type=video_type))
+#
